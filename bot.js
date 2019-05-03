06562fb5cab7c0102b958cdd845e7fe4bae48539
mido820 <42513373+mido820@users.noreply.github.com>
2019-05-04 00:17:33 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(ready`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size}' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`Hmmm`,"http://twitch.tv/Dream")
+client.user.setStatus("dnd")
+});
+
+client.login("NDYzMDkyOTE5NDY5OTk4MDkx.DhrYZA.eEdlu4gRuM5IOwSAUdKl03lv2BA");// لا تغير فيها شيء
